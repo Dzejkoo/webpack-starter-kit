7c8a09f0db98b28086ead2d427e91c00159a8ee0
Jakub Jóźwiak <jakub.lazy@gmail.com>
2021-07-07 20:27:40 +0200

    Add loader for css and sass also install plugin: CleanWebpackPlugin and HtmlWebpackPlugin
    Made separate file for sass and Js for better clean code also create folder template with index.html file

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,45 @@
 const paths = require('./paths');
-
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: [paths.src + '/index.js'],
+    entry: [paths.src + '/main.js'],
 
     output: {
         path: paths.bulid,
         filename: 'main.bundle.js'
-    }
+    },
+
+    module: {
+        rules: [{
+                test: /\.txt$/,
+                use: 'raw-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '',
+            template: 'src/template/index.html'
+        })
+    ]
 }
